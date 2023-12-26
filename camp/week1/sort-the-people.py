126d7af7dd8e7ab44060a0d99949cb1b81e28ec4
Enkutatash Eshetu <112767747+enkutatash@users.noreply.github.com>
2023-12-26 19:46:16 +0300

    Added solution for Sort the People

--- camp/week1/sort-the-people.py
+++ camp/week1/sort-the-people.py
@@ -1,12 +1,16 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
 
-        for i in range(len(heights)):
-            maxi=i
-            for j in range(i+1,len(heights)):
-                if heights[j]>heights[maxi]:
-                    maxi=j
-            heights[maxi],heights[i]=heights[i],heights[maxi]
-            names[maxi],names[i]=names[i],names[maxi]
+        for i in range(1,len(heights)):
+            j=i-1
+            while j>=0:
+                if heights[i]>heights[j]:
+                    heights[i],heights[j]=heights[j],heights[i]
+                    names[i],names[j]=names[j],names[i]
+                    i=j
+                else:
+                    break
+                j-=1
+            
         return names
         
