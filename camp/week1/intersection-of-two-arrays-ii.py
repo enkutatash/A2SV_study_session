db08405e224f5e070f836d0bc6bbb32cdaf8f7b0
Enkutatash Eshetu <112767747+enkutatash@users.noreply.github.com>
2023-12-28 20:28:05 +0300

    Added solution for Intersection of Two Arrays II

--- camp/week1/intersection-of-two-arrays-ii.py
+++ camp/week1/intersection-of-two-arrays-ii.py
@@ -1,14 +1,12 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        num1=Counter(nums1)
-        num2=Counter(nums2)
-        nums1=set(nums1)
-        nums2=set(nums2)
-        num3=nums1 and nums2
-        li=[]
-        for i in num3:
-            l=min(num1[i], num2[i])
-            li.extend([i]*l)
-        return li
-
-        
+        if len(nums1) > len(nums2):
+            return self.intersect(nums2, nums1)
+            
+        count = Counter(nums1)
+        ans = []
+        for x in nums2:
+            if count[x] > 0:
+                ans.append(x)
+                count[x] -= 1
+        return ans
