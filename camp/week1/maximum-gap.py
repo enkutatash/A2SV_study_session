31300334c82f311a30ccd6601f89e506ee9dcf4f
Enkutatash Eshetu <112767747+enkutatash@users.noreply.github.com>
2023-12-28 15:17:15 +0300

    Added solution for Maximum Gap

---
+++ camp/week1/maximum-gap.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        if len(nums)<2:
+            return 0
+        nums.sort()
+        maxi=float("-inf")
+        for i in range(1,len(nums)):
+            maxi=max(maxi,nums[i]-nums[i-1])
+        return maxi
+
+        
