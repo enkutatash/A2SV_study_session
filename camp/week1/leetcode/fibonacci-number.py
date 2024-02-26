11c7d6d599054df627991550d0dcb656604b27bb
Enkutatash Eshetu <112767747+enkutatash@users.noreply.github.com>
2024-02-26 10:47:11 +0300

    Add solution for Fibonacci Number

--- camp/week1/leetcode/fibonacci-number.py
+++ camp/week1/leetcode/fibonacci-number.py
@@ -1,7 +1,22 @@
+check={}
 class Solution:
     def fib(self, n: int) -> int:
-        if n<=1: 
+        if n <= 1: 
             return n
-        
-        return self.fib(n-1)+ self.fib(n-2)
+        first=0
+        second=0
+        if n-1 in check:
+            first=check[n-1]
+        else:
+            first= self.fib(n-1)
+            check[n-1]=first
+        if n-2 in check:
+            second= check[n-2]
+        else:
+            second=self.fib(n-2)
+            check[n-2]=second
+        return first + second
+            
+
+       
         
